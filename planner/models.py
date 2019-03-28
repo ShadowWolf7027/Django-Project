a94ba1269c8a16c5fdedddf73d3ed5e76404674a
Derek Nunn <danunn01@gmail.com>
2019-03-28 11:31:35 -0400

    Start

---
+++ planner/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+def calendar(django.db.models):
+    pass
+
+def months(django.db.models):
+    pass
+
+def days(django.db.models):
+    pass
+
+def events(django.db.models):
+    pass
+
+def fileStorage(django.db.models):
+    pass
---
+++ planner/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def calendar_view():
+    pass
+
+def months_view():
+    pass
+
+def days_view():
+    pass
+
+def events_view():
+    pass
+
+def fileStorage_view():
+    pass
